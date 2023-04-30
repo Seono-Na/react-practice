3c8925d58fa878f9af39f6c83d2640c7de513e11
Seono-Na <seono5670@gmail.com>
2023-04-30 13:13:27 +0900

    autocrcl

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import './App.css';
-import Counter from './components/Counter';
+import React, { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
 
 export default function AppCounter() {
   const [count, setCount] = useState(0);
   return (
-    <div className='container'>
-      <div className='banner'>
-        Total Count: {count} {count > 10 ? '🔥' : '❄️'}
+    <div className="container">
+      <div className="banner">
+        Total Count: {count} {count > 10 ? "🔥" : "❄️"}
       </div>
-      <div className='counters'>
+      <div className="counters">
         <Counter />
         <Counter />
       </div>
