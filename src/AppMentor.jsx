05decd3352a9df81c7c5036d8eea3ad1680a7771
Seono-Na <seono5670@gmail.com>
2023-05-05 17:16:03 +0900

    종합 객체 상태 관리

---
+++ src/AppMentor.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+export default function AppMentor() {
+  const [person, setPerson] = useState({
+    name: "서노",
+    title: "개발자",
+    mentor: {
+      name: "존",
+      title: "시니어개발자",
+    },
+  });
+  return (
+    <div>
+      <h1>
+        {person.name}은 {person.title}
+      </h1>
+      <p>
+        {person.name}의 멘토는 {person.mentor.name}({person.mentor.title})
+      </p>
+      <button
+        onClick={() => {
+          const name = prompt(`What's your mentor's name?`);
+        }}
+      >
+        멘토 이름 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          const title = prompt(`What's your mentor's title?`);
+        }}
+      >
+        멘토 타이틀 바꾸기
+      </button>
+    </div>
+  );
+}
